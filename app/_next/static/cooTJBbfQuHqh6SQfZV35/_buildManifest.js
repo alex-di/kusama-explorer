5c35db63f6d3fc145363a1a35efbce173f93fbc5
Diakov, Aleksei <a.diakov@fxpro.com>
2020-10-18 23:44:27 +0300

    Use vercel

--- app/_next/static/cooTJBbfQuHqh6SQfZV35/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F4cbcddab.ac573a80bb7f22ac3743.js","static\u002Fchunks\u002Fpages\u002Findex-425df048a376fd694d5a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-50207653ddf143cc6e6e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
